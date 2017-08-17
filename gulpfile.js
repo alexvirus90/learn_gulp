008c493517a5f727653022d8cf22ecb120653694
alex <a.kom90@mail.ru>
2017-08-17 23:24:38 +0300

    Use plugin cached, remember.

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,55 @@
 'use strict';
 
 const gulp = require('gulp');
-const stylus = require('gulp-stylus');
-const sourcemaps = require('gulp-sourcemaps');
+const concat = require('concat');
+const autoprefixer = require('autoprefixer');
+const remember = require('gulp-remember');
+const path = require('path');
+const cached = require('gulp-cached');
 const debug = require('gulp-debug');
-const gulpIf = require('gulp-if');
-const del = require('del');
-const newer = require('gulp-newer');
-
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+// const del = require('del');
+// const newer = require('gulp-newer');
+// const stylus = require('gulp-stylus');
+// const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function () {
 
-	return gulp.src('frontend/styles/main.styl')
-	  .pipe(gulpIf(isDevelopment, sourcemaps.init()))
-	  .pipe(stylus())
-	  .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+	return gulp.src('frontend/styles/**/*.css')
+	  .pipe(cached('styles'))
+	  .pipe(autoprefixer())
+	  .pipe(remember('styles'))
+	  .pipe(concat('all.css'))
 	  .pipe(gulp.dest('public'));
+  // .pipe(stylus('all.css'))
 
 });
 
-gulp.task('clean', function () {
-  return del('public');
-});
+// gulp.task('clean', function () {
+//   return del('public');
+// });
 
-gulp.task('assets', function () {
-  return gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')})
-	.pipe(newer('public'))
-	.pipe(debug({title: 'assets'}))
-	.pipe(gulp.dest('public'));
-});
+// gulp.task('assets', function () {
+//   return gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')})
+// 	.pipe(newer('public'))
+// 	.pipe(debug({title: 'assets'}))
+// 	.pipe(gulp.dest('public'));
+// });
 
-gulp.task('build', gulp.series(
-  'clean',
-  gulp.parallel('styles', 'assets'))
-);
+// gulp.task('build', gulp.series(
+//   'clean',
+//   gulp.parallel('styles'/*, 'assets'*/))
+// );
 
 gulp.task('watch', function () {
-  gulp.watch('frontend/styles/**/*.*', gulp.series('styles'));
+  gulp.watch('frontend/styles/**/*.css', gulp.series('styles')).on('unlink', function (filepath) {
+	remember.forget('styles', path.resolve(filepath));
+	delete cached.caches.styles[path.resolve(filepath)];
+  });
 
-  gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
+  // gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
 });
 
-gulp.task('dev', gulp.series('build', 'watch'));
+gulp.task('dev', gulp.series('styles', 'watch'));
 
 
 
