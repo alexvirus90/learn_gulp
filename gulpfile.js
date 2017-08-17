30bc14bb90e1709c89bd6eb8c10eb12b107d4846
alex <a.kom90@mail.ru>
2017-08-17 22:17:12 +0300

    Use modul del, plugin gulp-debug, gulp-sourcemaps, gulp--if.

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,32 @@ const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 const sourcemaps = require('gulp-sourcemaps');
 const debug = require('gulp-debug');
+const gulpIf = require('gulp-if');
+const del = require('del');
 
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 gulp.task('styles', function () {
 
-	let pipeline = gulp.src('frontend/styles/main.styl');
-
-	if (isDevelopment) {
-	  pipeline = pipeline.pipe(sourcemaps.init());
-	}
-
-	pipeline = pipeline
-	  .pipe(stylus());
-
-	if (isDevelopment) {
-	  pipeline = pipeline.pipe(sourcemaps.write());
-	}
-
-	return pipeline
+	return gulp.src('frontend/styles/main.styl')
+	  .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+	  .pipe(stylus())
+	  .pipe(gulpIf(isDevelopment, sourcemaps.write()))
 	  .pipe(gulp.dest('public'));
 
 });
+
+gulp.task('clean', function () {
+  return del('public');
+});
+
+gulp.task('assets', function () {
+  return gulp.src('frontend/assets/**')
+	.pipe(gulp.dest('public'));
+});
+
+gulp.task('build', gulp.series(
+  'clean',
+  gulp.parallel('styles', 'assets'))
+);
 
