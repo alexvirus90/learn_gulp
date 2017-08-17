572aec9bb4810bd063e2fa0ead9412bd529e054f
alex <a.kom90@mail.ru>
2017-08-17 21:02:46 +0300

    Start build styles.

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const sourcemaps = require('gulp-sourcemaps');
+const debug = require('gulp-debug');
+
+
+gulp.task('styles', function () {
+
+	return gulp.src('frontend/styles/main.styl')
+	  .pipe(sourcemaps.init())
+	  .pipe(stylus())
+	  .pipe(sourcemaps.write())
+	  .pipe(gulp.dest('public'));
+
+});
+
